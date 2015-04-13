7843254e97d49908a94532809f1ae9e1d925d339
MatheusGodinho <matheus.godinho@outlook.com>
2015-04-12 21:15:30 -0300

    adding math module with glm

---
+++ opengl-engine/src/math/Math.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using vec2 = glm::vec2;
+using vec3 = glm::vec3;
+using vec4 = glm::vec4;
+using mat4 = glm::mat4;
